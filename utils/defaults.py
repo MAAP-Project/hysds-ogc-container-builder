732a5a0c3dc49908e5f71a34f37e0c37bb23391f
Sujen Shah <sujen1412@users.noreply.github.com>
2025-12-09 14:23:00 -0800

    Update JOB_SPEC with new worker file path

--- utils/defaults.py
+++ utils/defaults.py
@@ -23,7 +23,7 @@ JOB_SPEC_INPUT_MAP = {
 
 JOB_SPEC = {
     "imported_worker_files": {
-        "$HOME/verdi/etc/maap-dps.env": "/maap-dps.env",
+        "/data/work/etc/maap-dps.env": "/maap-dps.env",
         "/tmp": ["/tmp", "rw"]
     },
     "soft_time_limit": 86400,
